54be8e230bce471844399ab1d0336dc1ab72b422
Alex Lyapko <alexxx.ax4@gmail.com>
2018-10-08 16:48:53 +0300

    added md5 with salt

--- controllers/login.php
+++ controllers/login.php
@@ -1,12 +1,13 @@
 <?php
 
 $view = 'login';
+$salt = '12345';
 
 $username = $_POST['username'] ?? null;
-$password = $_POST['password'] ?? null;
+$password = md5($_POST['password'].$salt) ?? null;
 
 if ($username) {
-    if ($username === $user['username'] && $password === $user['password']) {
+    if ($username === $user['username'] && $password === md5($user['password'].$salt)) {
         $_SESSION['username'] = $username;
     } else {
         $vars['error'] = 'Username and Password mismatch';
